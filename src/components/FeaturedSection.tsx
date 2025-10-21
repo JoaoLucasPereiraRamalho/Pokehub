2a52d697d47cb04742b50a750349e1055f230f46
Joao-Lucas-Ramalho <joao.lucas@hotsales.com.br>
2025-10-21 15:48:06 -0300

    feat: implementar layout e estilização da featured section

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -1,24 +1,26 @@
 function FeaturedSection() {
   return (
-    <div className="bg-success">
-      <div className="d-flex m-5">
-        <div className="w-50">
-          <h1>Featured Section</h1>
-          <h5>Descubra Pokemons Incríveis!</h5>
-          <h6>
-            Explore uma seleção especial de Pokémons com habilidades únicas e
-            histórias fascinantes. Encontre seu próximo companheiro de aventura
-            aqui!
-          </h6>
+    <div className="bg-light">
+      <div className="d-flex text-primary">
+        <div className="m-5 w-50">
+          <div className="h-75">
+            <h1 className="display-2">Novidade</h1>
+            <h5>Descubra Pokemons Incríveis!</h5>
+            <h6>
+              Explore uma seleção especial de Pokémons com habilidades únicas e
+              histórias fascinantes. Encontre seu próximo companheiro de
+              aventura aqui!
+            </h6>
+          </div>
           <div className="d-flex justify-content-center">
-            <button className="btn btn-outline-light text-white bg-primary border-0 rounded-pill mt-3 py-3 px-4">
-              Saiba Mais
-            </button>
+            <button className="btn-linear-2">Saiba Mais</button>
           </div>
         </div>
         <div className="d-flex w-50 bg-light">
-          <h1>Imagem</h1>
-          <img src="" />
+          <img
+            className="w-100 mx-auto d-block"
+            src="/src/assets/imagem1_feature_section.jpeg"
+          />
         </div>
       </div>
     </div>
