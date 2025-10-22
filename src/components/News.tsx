bc7bf25f3f63ac1e8ffe55aa7b7aac7d36b13efc
Joao-Lucas-Ramalho <joao.lucas@hotsales.com.br>
2025-10-22 10:13:39 -0300

    feat: implementar layout e estilização das news

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,34 +1,51 @@
 function News() {
   return (
-    <div className="m-5">
-      <div className="w-100 d-flex justify-content-center mb-5">
-        <h1>Noticias</h1>
+    <div style={{ backgroundColor: "#091D3C" }} className="">
+      <div className="py-5 d-flex justify-content-center">
+        <div className="bg-light w-75 d-flex justify-content-center mb-5">
+          <h1>Noticias</h1>
+        </div>
       </div>
 
-      <div className="d-flex gap-5">
-        <div className="w-50 bg-light d-flex flex-column gap-2">
+      <div className="d-flex gap-5 w-75 mx-auto py-4">
+        <div
+          style={{ backgroundColor: "#091D3C" }}
+          className="w-50 d-flex flex-column gap-2"
+        >
           <div className="d-flex gap-2">
             <div className="w-50 bg-primary">
-              <img src="" />
+              <img
+                className="w-100 h-100"
+                src="/src/assets/imagem1_feature_section.jpeg"
+              />
             </div>
             <div className="w-50 bg-success">
-              <img src="" />
+              <img
+                className="w-100 h-100"
+                src="/src/assets/imagem1_feature_section.jpeg"
+              />
             </div>
           </div>
 
           <div className="w-100 bg-warning">
-            <img src="" />
+            <img
+              className="w-100 h-100"
+              src="/src/assets/haunter_sem_fundo.png"
+            />
           </div>
         </div>
 
-        <div className="w-50 bg-primary">
-          <h2>Mais Lidas</h2>
-          <h4>Titulo Noticia 1</h4>
-          <img src="" />
-          <h6>Resumo Noticia 1</h6>
-          <button className="btn btn-outline-light text-white bg-dark border-0 rounded-pill mt-3 py-2 px-4">
-            Ler mais
-          </button>
+        <div className="w-50 bg-light p-4 d-flex flex-column align-items-center justify-content-center text-center">
+          <h2>Elas chegaram!</h2>
+          <h4>Novas megas evoluções chegam ao jogo Zn</h4>
+          <img className="m-5" src="/src/assets/imagem1_feature_section.jpeg" />
+          <h6 className="text-justify mb-4">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen.
+          </h6>
+          <button className="btn-linear-2">Ler mais</button>
         </div>
       </div>
     </div>
